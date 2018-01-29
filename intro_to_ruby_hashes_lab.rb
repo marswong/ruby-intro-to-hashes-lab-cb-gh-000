0619c32dc280c2c1226f9af6370ec2c6d2a23281
Mars Wong <marswong618@gmail.com>
2018-01-29 03:48:44 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,13 +24,15 @@ def monopoly_with_third_tier
   monopoly = {
     railsroads: {
       :pieces: 4,
-      :pennsylvania_railroad: {},
+      :names: {
+        :reading_railroad: {},
+        :pennsylvania_railroad: {}
+      },
       :rent_in_dollars: {
         :one_piece_owned: 25,
         :two_pieces_owned: 50,
         :three_pieces_owned: 100,
-        :four_pieces_owned: 200,
-        :reading_railroad: {}
+        :four_pieces_owned: 200
       }
     }
   }
